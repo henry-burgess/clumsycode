399136bfc59a29997b39ec628642ce5e4f02b4ac
Henry Burgess <henryjburg@gmail.com>
2021-12-22 15:08:33 -0600

    Add new post
    Update post link layout.
    Update 404 content.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# clumsycode
-The code for my personal blog.
+# Clumsy Code
+
+This is the source code for my personal website / blog at [clumsycode.com](https://clumsycode.com)!
+
+## Develop
+
+Install the dependencies with `yarn`, then run a in-browser preview with `yarn start` or `yarn develop`.
+
+Generate a production build with `yarn build`.
+
+Built using [Gatsby](https://www.gatsbyjs.com) 💜
--- src/templates/post.js
+++ src/templates/post.js
@@ -74,16 +74,16 @@ const Post = ({ data, location }) => {
           }}
         >
           <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                ← {next.frontmatter.title}
+            {previous && (
+              <Link to={previous.fields.slug} rel="prev">
+                ← {previous.frontmatter.title}
               </Link>
             )}
           </li>
           <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                {previous.frontmatter.title} →
+            {next && (
+              <Link to={next.fields.slug} rel="next">
+                {next.frontmatter.title} →
               </Link>
             )}
           </li>
